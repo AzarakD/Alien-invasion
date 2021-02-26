6b0fc63d82215c6e0eb1f3495b4a57042c81bf5b
Azarak <abbatd@gmail.com>
2021-02-27 02:21:00 +1000

    added x movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,9 +31,14 @@ class AlineInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
